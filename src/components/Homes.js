ccef26cf85dc7538fb331d4c2e56ea6db3b1304e
protokol110 <asinakevic@gmail.com>
2021-07-29 14:27:34 +0300

    test

--- src/components/Homes.js
+++ src/components/Homes.js
@@ -3,12 +3,12 @@ import React from 'react';
 import HomesHeader from './HomesHeader';
 import HomesContent from './HomesContent';
 
-function Homes(props) {
+function Homes() {
   return (
     <div className="block-homes">
       <HomesHeader />
       <div className="block-homes__content">
-        <HomesContent data={props.data} />
+        <HomesContent />
       </div>
     </div>
   );
--- src/components/HomesContent.js
+++ src/components/HomesContent.js
@@ -1,7 +1,69 @@
 import React from 'react';
-import data from './constants/array';
 
-function HomesContent() {
+export default class HomesContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://fe-student-api.herokuapp.com/api/hotels/popular')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        },
+      );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return (
+        <p>
+          Error
+          {error.message}
+        </p>
+      );
+    } if (!isLoaded) {
+      return <p>Loading...</p>;
+    }
+    return (items.map((elem) => (
+      <div className="block-homes__container-image" key={elem.id}>
+        <div className="block-homes__container-image-first block-1-hotels">
+          <img
+            src={elem.imageUrl}
+            alt={elem.name}
+            className="image-1-hotels"
+          />
+          <p className="block-homes__container-text">
+            {elem.name}
+          </p>
+          <p className="block-homes__container-text-grey">
+            {elem.city}
+            ,
+            {elem.country}
+          </p>
+        </div>
+      </div>
+    ))
+    );
+  }
+}
+/* function HomesContent() {
   return (data.map((elem) => (
     <div className="block-homes__container-image" key={elem.id}>
       <div className="block-homes__container-image-first block-1-hotels">
@@ -22,5 +84,4 @@ function HomesContent() {
     </div>
   ))
   );
-}
-export default HomesContent;
+} */
